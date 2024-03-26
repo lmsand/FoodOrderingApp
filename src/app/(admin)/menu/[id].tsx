b647697b4b7311f8941562f9c4e8ed3401c29653
lmsand <lmsand99@icloud.com>
2024-03-26 15:04:45 -0500

    update and delete UI

--- src/app/(admin)/menu/[id].tsx
+++ src/app/(admin)/menu/[id].tsx
@@ -1,12 +1,14 @@
 import { View, Text, Image, StyleSheet, Pressable } from "react-native";
 import React from "react";
-import { Stack, useLocalSearchParams, useRouter } from "expo-router";
+import { Stack, useLocalSearchParams, useRouter, Link } from "expo-router";
 import products from "@/assets/data/products";
 import { defaultPizzaImage } from "@/src/components/ProductListItem";
 import { useState } from "react";
 import Button from "@/src/components/Button";
 import { useCart } from "@/src/providers/CartProvider";
 import { PizzaSize } from "@/src/types";
+import Colors from "@/src/constants/Colors";
+import { FontAwesome } from "@expo/vector-icons";
 
 const sizes: PizzaSize[] = ["S", "M", "L", "XL"];
 
@@ -34,6 +36,25 @@ const ProductDetailsScreen = () => {
 
   return (
     <View style={styles.container}>
+      <Stack.Screen
+        options={{
+          title: "Menu",
+          headerRight: () => (
+            <Link href={`/(admin)/menu/create?id=${id}`} asChild>
+              <Pressable>
+                {({ pressed }) => (
+                  <FontAwesome
+                    name="pencil"
+                    size={25}
+                    color={Colors.light.tint}
+                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
+                  />
+                )}
+              </Pressable>
+            </Link>
+          ),
+        }}
+      />
       <Stack.Screen options={{ title: product.name }} />
       <Image
         source={{ uri: product.image || defaultPizzaImage }}
--- src/app/(admin)/menu/create.tsx
+++ src/app/(admin)/menu/create.tsx
@@ -1,16 +1,19 @@
 import Button from "@/src/components/Button";
-import { View, Text, StyleSheet, TextInput, Image } from "react-native";
+import { View, Text, StyleSheet, TextInput, Image, Alert } from "react-native";
 import { useState } from "react";
 import { defaultPizzaImage } from "@/src/components/ProductListItem";
 import Colors from "@/src/constants/Colors";
 import * as ImagePicker from 'expo-image-picker';
-import { Stack } from "expo-router";
+import { Stack, useLocalSearchParams } from "expo-router";
 
 const CreateProductScreen = () => {
   const [name, setName] = useState("");
   const [price, setPrice] = useState("");
   const [errors, setErrors] = useState("")
   const [image, setImage] = useState<string | null>(null);
+
+  const { id } = useLocalSearchParams()
+  const isUpdating = !!id // true if isUpdating is defined, need id if trying to update item
 
   const resetFields = () => {
     setName('')
@@ -37,10 +40,31 @@ const CreateProductScreen = () => {
     return true
   }
 
+  const onSubmit = () => {
+    if (isUpdating) {
+      // update
+      onUpdateCreate()
+    } else {
+      onCreate()
+    }
+  }
+
   const onCreate = () => {
     if (!validateInput()) {
       return
     }
+
+    // save in the database
+
+    resetFields()
+  };
+
+  const onUpdateCreate = () => {
+    if (!validateInput()) {
+      return
+    }
+
+    console.warn("updating product")
 
     // save in the database
 
@@ -63,9 +87,26 @@ const CreateProductScreen = () => {
     }
   };
 
+  const onDelete = () => {
+    console.warn("Delete!!!!")
+  }
+
+  const confirmDelete = () => {
+    Alert.alert('Confirm', 'Are you sure you want to delete this product?', [
+      {
+        text: 'Cancel'
+      },
+      {
+        text: 'Delete',
+        style: 'destructive',
+        onPress: onDelete
+      }
+    ])
+  }
+
   return (
     <View style={styles.container}>
-      <Stack.Screen options={{ title: "Create Product" }}/>
+      <Stack.Screen options={{ title: isUpdating ? "Update Product" : "Create Product" }}/>
 
       <Image source={{ uri: image || defaultPizzaImage}} style={styles.image} />
       <Text onPress={pickImage} style={styles.textButton}>Select Image</Text>
@@ -88,7 +129,8 @@ const CreateProductScreen = () => {
       />
 
       <Text style={{ color: 'red' }}>{errors}</Text>
-      <Button onPress={onCreate} text="Create" />
+      <Button onPress={onSubmit} text={ isUpdating ? "Update" : "Create" }/>
+      {isUpdating && <Text onPress={confirmDelete} style={styles.textButton}>Delete</Text>}
     </View>
   );
 };
@@ -113,7 +155,8 @@ const styles = StyleSheet.create({
   image: {
     width: '50%',
     aspectRatio: 1,
-    alignSelf: 'center'
+    alignSelf: 'center',
+    borderRadius: 100
   },
   textButton: {
     alignSelf: 'center',
