217e62dcf5f64ff4763affa4f649027ab5375fb3
lmsand <lmsand99@icloud.com>
2024-03-27 14:12:51 -0500

    order status selector

--- src/app/(admin)/orders/[id].tsx
+++ src/app/(admin)/orders/[id].tsx
@@ -1,8 +1,10 @@
-import { FlatList, Text, View } from "react-native";
+import { FlatList, Text, View, Pressable } from "react-native";
 import { useLocalSearchParams, Stack } from "expo-router";
 import orders from "@/assets/data/orders";
 import OrderItemListItem from "@/src/components/OrderItemListItem";
 import OrderListItem from "@/src/components/OrderListItem";
+import { OrderStatusList } from "@/src/types";
+import Colors from "@/src/constants/Colors";
 
 export default function OrderDetailsScreen() {
   const { id } = useLocalSearchParams();
@@ -24,6 +26,38 @@ export default function OrderDetailsScreen() {
         contentContainerStyle={{ gap: 10 }}
         // ListHeaderComponent={() => <OrderListItem order={order} />} top nav
         // ListFooterComponent={() => <OrderListItem order={order} />} footer nav
+        ListFooterComponent={() => <>
+          <Text style={{ fontWeight: 'bold' }}>Status</Text>
+          <View style={{ flexDirection: 'row', gap: 5 }}>
+            {OrderStatusList.map((status) => (
+              <Pressable
+                key={status}
+                onPress={() => console.warn('Update status')}
+                style={{
+                  borderColor: Colors.light.tint,
+                  borderWidth: 1,
+                  padding: 10,
+                  borderRadius: 5,
+                  marginVertical: 10,
+                  backgroundColor:
+                    order.status === status
+                      ? Colors.light.tint
+                      : 'transparent',
+                }}
+              >
+                <Text
+                  style={{
+                    color:
+                      order.status === status ? 'white' : Colors.light.tint,
+                  }}
+                >
+                  {status}
+                </Text>
+              </Pressable>
+            ))}
+          </View>
+        </>
+        }
       />
     </View>
   );
