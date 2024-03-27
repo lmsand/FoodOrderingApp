a39a51740f1cc86dfc82a712db021ae3a6d73fe6
lmsand <lmsand99@icloud.com>
2024-03-27 14:33:30 -0500

    top tab navigator

--- src/app/(admin)/orders/_layout.tsx
+++ src/app/(admin)/orders/_layout.tsx
@@ -3,7 +3,8 @@ import { Stack, } from "expo-router"
 export default function Menu() {
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ title: 'Orders' }} />
+      {/* <Stack.Screen name="index" options={{ title: 'Orders' }} /> */}
+      <Stack.Screen name="list" options={{ headerShown: false }} />
     </Stack>
   )
 }
